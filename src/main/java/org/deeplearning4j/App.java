8df45ef1673bc2dc8f618d5f3edc8b5c9569cccd
Adam Gibson <adam@skymind.io>
2015-06-19 08:52:31 -0700

    add gitignore

---
+++ src/main/java/org/deeplearning4j/App.java
@@ -0,0 +1,22 @@
+package org.deeplearning4j;
+
+import org.deeplearning4j.berkeley.StringUtils;
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) throws  Exception {
+        Word2Vec vec = WordVectorSerializer.loadGoogleModel(new File(args[0]),Boolean.parseBoolean(args[1]));
+        List<String> words = StringUtils.split(args[2],",");
+        for(String s : words) {
+            System.out.println(vec.wordsNearest(s,20));
+        }
+    }
+}

6232494c53457956dfe920dbc70435ad94918385
Adam Gibson <adam@skymind.io>
2015-06-27 14:11:41 -0700

    add readme

--- src/main/java/org/deeplearning4j/App.java
+++ src/main/java/org/deeplearning4j/App.java
@@ -3,8 +3,7 @@ package org.deeplearning4j;
 import org.deeplearning4j.berkeley.StringUtils;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.word2vec.Word2Vec;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.ops.transforms.Transforms;
+
 
 import java.io.File;
 import java.util.List;
@@ -17,16 +16,8 @@ public class App {
     public static void main( String[] args ) throws  Exception {
         Word2Vec vec = WordVectorSerializer.loadGoogleModel(new File(args[0]),Boolean.parseBoolean(args[1]));
         List<String> words = StringUtils.split(args[2],",");
-
         for(String s : words) {
-            INDArray vecFirst = vec.getWordVectorMatrix(s);
-            INDArray noOffset = vecFirst.dup();
-            for(String s2 : words) {
-                INDArray vecSecond = vec.getWordVectorMatrix(s2);
-                INDArray noOffsetSecond = vecSecond.dup();
-                System.out.println("Offset " + s + " " + s2 + " " + Transforms.cosineSim(vecFirst,vecSecond));
-                System.out.println("No Offset " + s + " " + s2 + " " + Transforms.cosineSim(noOffset,noOffsetSecond));
-            }
+            System.out.println("Words nearest " + vec.wordsNearest(s,20));
 
         }
     }
